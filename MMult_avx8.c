5dda207db48dbe52bf5945025ec5975ea6411395
slamdunk1214 <1192092805@qq.com>
2018-08-17 23:21:03 +0800

    checked avx8

--- MMult_avx8.c
+++ MMult_avx8.c
@@ -26,7 +26,6 @@ void MY_MMult( int m, int n, int k, double *a, int lda,
 
 /* This time, we compute a mc x n block of C by a call to the InnerKernel */
 
-	double *tmp = (double*)malloc(4*sizeof(double));
 	
     for ( p=0; p<k; p+=kc ){
 		pb = min( k-p, kc );
@@ -76,8 +75,11 @@ void PackMatrixA( int k, double *a, int lda, double *a_to )
 		*(a_to+1) = *(a_ij_pntr+1);
 		*(a_to+2) = *(a_ij_pntr+2);
 		*(a_to+3) = *(a_ij_pntr+3);
-		
-		a_to += 4;
+		*(a_to+4) = *(a_ij_pntr+4);
+		*(a_to+5) = *(a_ij_pntr+5);
+		*(a_to+6) = *(a_ij_pntr+6);
+		*(a_to+7) = *(a_ij_pntr+7);
+		a_to += 8;
 	}
 }
 
@@ -86,13 +88,19 @@ void PackMatrixB( int k, double *b, int ldb, double *b_to )
 	int i;
 	double 
 		*b_i0_pntr = &B( 0, 0 ), *b_i1_pntr = &B( 0, 1 ),
-		*b_i2_pntr = &B( 0, 2 ), *b_i3_pntr = &B( 0, 3 );
+		*b_i2_pntr = &B( 0, 2 ), *b_i3_pntr = &B( 0, 3 ),
+		*b_i4_pntr = &B( 0, 4 ), *b_i5_pntr = &B( 0, 5 ),
+		*b_i6_pntr = &B( 0, 6 ), *b_i7_pntr = &B( 0, 7 );
 	
 	for( i=0; i<k; i++){  /* loop over rows of B */
 		*b_to++ = *b_i0_pntr++;
 		*b_to++ = *b_i1_pntr++;
 		*b_to++ = *b_i2_pntr++;
 		*b_to++ = *b_i3_pntr++;
+		*b_to++ = *b_i4_pntr++;
+		*b_to++ = *b_i5_pntr++;
+		*b_to++ = *b_i6_pntr++;
+		*b_to++ = *b_i7_pntr++;
 	}
 }
 
@@ -101,19 +109,14 @@ void PackMatrixB( int k, double *b, int ldb, double *b_to )
 #include <pmmintrin.h>  // SSE2
 #include <emmintrin.h>  // SSE3
 #include <immintrin.h>
-
-typedef union
-{
-  __m128d v;
-  double d[2];
-} v2df_t;
-
+#include <stdio.h>
 typedef union
 {
   __m256d v;
   double d[4];
 } v4df_t;
 
+void
 inspect(__m256d v)
 {
     double d[4];
@@ -121,45 +124,31 @@ inspect(__m256d v)
     printf( "[%lf, %lf, %lf, %lf]\n", d[0], d[1], d[2], d[3]);
 }
 
+static void inline
+inspect8(__m256d v1, __m256d v2)
+{
+    double d[8];
+    _mm256_storeu_pd( d    , v1 );
+    _mm256_storeu_pd( d + 4, v2 );
+    printf( "[%f,%f,%f,%f] [%f,%f,%f,%f]\n", 
+	d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7] );
+}
 
-inline void
-transpose8(__m256d *r0, __m256d *r1, __m256d *r2, __m256d *r3, __m256d *r4, __m256d *r5, __m256d *r6, __m256d *r7,__m256d *r8, __m256d *r9, __m256d *ra, __m256d *rb, __m256d *rc, __m256d *rd, __m256d *re, __m256d *rf){
+static void inline
+transpose(__m256d *v0, __m256d *v1, __m256d *v2, __m256d *v3){
+
+    __m256d t0, t1, t2, t3;
     
-    __m256d t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, ta, tb, tc, td, te, tf;
+    t0 = _mm256_unpacklo_pd( *v0 , *v1 );
+    t1 = _mm256_unpackhi_pd( *v0 , *v1 );
+    t2 = _mm256_unpacklo_pd( *v2 , *v3 );
+    t3 = _mm256_unpackhi_pd( *v2 , *v3 );
+    
+    *v0 = _mm256_permute2f128_pd( t0, t2, 0x20);
+    *v1 = _mm256_permute2f128_pd( t1, t3, 0x20);
+    *v2 = _mm256_permute2f128_pd( t0, t2, 0x31);
+    *v3 = _mm256_permute2f128_pd( t1, t3, 0x31);
 
-    t0 = _mm256_unpacklo_pd( *r0 , *r1 );
-    t1 = _mm256_unpackhi_pd( *r0 , *r1 );
-    t2 = _mm256_unpacklo_pd( *r2 , *r3 );
-    t3 = _mm256_unpackhi_pd( *r2 , *r3 );
-    t4 = _mm256_unpacklo_pd( *r4 , *r5 );
-    t5 = _mm256_unpackhi_pd( *r4 , *r5 );
-    t6 = _mm256_unpacklo_pd( *r6 , *r7 );
-    t7 = _mm256_unpackhi_pd( *r6 , *r7 );
-    t8 = _mm256_unpacklo_pd( *r8 , *r9 );
-    t9 = _mm256_unpackhi_pd( *r8 , *r9 );
-    ta = _mm256_unpacklo_pd( *ra , *rb );
-    tb = _mm256_unpackhi_pd( *ra , *rb );
-    tc = _mm256_unpacklo_pd( *rc , *rd );
-    td = _mm256_unpackhi_pd( *rc , *rd );
-    te = _mm256_unpacklo_pd( *re , *rf );
-    tf = _mm256_unpackhi_pd( *re , *rf );
-
-    *r0 = _mm256_permute2f128_pd( t0, t2, 0x20);
-    *r1 = _mm256_permute2f128_pd( t1, t3, 0x20);
-    *r2 = _mm256_permute2f128_pd( t0, t2, 0x31);
-    *r3 = _mm256_permute2f128_pd( t1, t3, 0x31);
-    *r4 = _mm256_permute2f128_pd( t4, t6, 0x20);
-    *r5 = _mm256_permute2f128_pd( t5, t7, 0x20);
-    *r6 = _mm256_permute2f128_pd( t4, t6, 0x31);
-    *r7 = _mm256_permute2f128_pd( t5, t7, 0x31);
-    *r8 = _mm256_permute2f128_pd( t8, ta, 0x20);
-    *r9 = _mm256_permute2f128_pd( t9, tb, 0x20);
-    *ra = _mm256_permute2f128_pd( t8, ta, 0x31);
-    *rb = _mm256_permute2f128_pd( t9, tb, 0x31);
-    *rc = _mm256_permute2f128_pd( tc, te, 0x20);
-    *rd = _mm256_permute2f128_pd( tc, te, 0x20);
-    *re = _mm256_permute2f128_pd( td, tf, 0x31);
-    *rf = _mm256_permute2f128_pd( td, tf, 0x31);
 }
 
 void AddDot8x8_avx( int k, double *a, int lda,  double *b, int ldb, double *c, int ldc )
@@ -210,7 +199,7 @@ void AddDot8x8_avx( int k, double *a, int lda,  double *b, int ldb, double *c, i
 		b_p7_vreg.v = _mm256_set1_pd( * (b+7) );   /* load and duplicate */
 		
 		b += 8;
-		
+
 		c_00_c_30_vreg.v = _mm256_add_pd( c_00_c_30_vreg.v,
 						   _mm256_mul_pd( a_0p_a_3p_vreg.v, b_p0_vreg.v));
 		c_01_c_31_vreg.v = _mm256_add_pd( c_01_c_31_vreg.v,
@@ -248,14 +237,6 @@ void AddDot8x8_avx( int k, double *a, int lda,  double *b, int ldb, double *c, i
                            _mm256_mul_pd( a_4p_a_7p_vreg.v, b_p7_vreg.v));
 
 	}
-
-
-
-
-
-
-
-
 
 /*
 	C( 0, 0 ) += c_00_c_30_vreg.d[0];  C( 0, 1 ) += c_01_c_31_vreg.d[0];
@@ -299,59 +280,79 @@ void AddDot8x8_avx( int k, double *a, int lda,  double *b, int ldb, double *c, i
 	C( 7, 6 ) += c_46_c_76_vreg.d[3];  C( 7, 7 ) += c_47_c_77_vreg.d[3];
 */
    
-    inspect( c_00_c_30_vreg.v );
-    inspect( c_01_c_31_vreg.v );
-    inspect( c_02_c_32_vreg.v );
-    inspect( c_03_c_33_vreg.v );
-    inspect( c_04_c_34_vreg.v );
-    inspect( c_05_c_35_vreg.v );
-    inspect( c_06_c_36_vreg.v );
-    inspect( c_07_c_37_vreg.v );
-    inspect( c_40_c_70_vreg.v );
-    inspect( c_41_c_71_vreg.v );
-    inspect( c_42_c_72_vreg.v );
-    inspect( c_43_c_73_vreg.v );
-    inspect( c_44_c_74_vreg.v );
-    inspect( c_45_c_75_vreg.v );
-    inspect( c_46_c_76_vreg.v );
-    inspect( c_47_c_77_vreg.v );
+//    inspect8( c_00_c_30_vreg.v, c_40_c_70_vreg.v );
+//    inspect8( c_01_c_31_vreg.v, c_41_c_71_vreg.v );
+//    inspect8( c_02_c_32_vreg.v, c_42_c_72_vreg.v );
+//    inspect8( c_03_c_33_vreg.v, c_43_c_73_vreg.v );
+//	
+//    inspect8( c_04_c_34_vreg.v, c_44_c_74_vreg.v );
+//    inspect8( c_05_c_35_vreg.v, c_45_c_75_vreg.v );
+//    inspect8( c_06_c_36_vreg.v, c_46_c_76_vreg.v );
+//    inspect8( c_07_c_37_vreg.v, c_47_c_77_vreg.v );
+		
+	transpose( &( c_00_c_30_vreg.v ), &( c_01_c_31_vreg.v ),
+			   &( c_02_c_32_vreg.v ), &( c_03_c_33_vreg.v ) );
 
-    transpose8( &( c_00_c_30_vreg.v ), &( c_40_c_70_vreg.v ),
-                &( c_01_c_31_vreg.v ), &( c_41_c_71_vreg.v ),
-                &( c_02_c_32_vreg.v ), &( c_42_c_72_vreg.v ),
-                &( c_03_c_33_vreg.v ), &( c_43_c_73_vreg.v ),
-                &( c_04_c_34_vreg.v ), &( c_44_c_74_vreg.v ),
-                &( c_05_c_35_vreg.v ), &( c_45_c_75_vreg.v ),
-                &( c_06_c_36_vreg.v ), &( c_46_c_76_vreg.v ),
-                &( c_07_c_37_vreg.v ), &( c_47_c_77_vreg.v ) );
+	transpose( &( c_40_c_70_vreg.v ), &( c_41_c_71_vreg.v ),
+			   &( c_42_c_72_vreg.v ), &( c_43_c_73_vreg.v ) );
+	
+	transpose( &( c_04_c_34_vreg.v ), &( c_05_c_35_vreg.v ),
+			   &( c_06_c_36_vreg.v ), &( c_07_c_37_vreg.v ) );
 
-    inspect( c_00_c_30_vreg.v );
-    inspect( c_01_c_31_vreg.v );
-    inspect( c_02_c_32_vreg.v );
-    inspect( c_03_c_33_vreg.v );
-    inspect( c_04_c_34_vreg.v );
-    inspect( c_05_c_35_vreg.v );
-    inspect( c_06_c_36_vreg.v );
-    inspect( c_07_c_37_vreg.v );
-    inspect( c_40_c_70_vreg.v );
-    inspect( c_41_c_71_vreg.v );
-    inspect( c_42_c_72_vreg.v );
-    inspect( c_43_c_73_vreg.v );
-    inspect( c_44_c_74_vreg.v );
-    inspect( c_45_c_75_vreg.v );
-    inspect( c_46_c_76_vreg.v );
-    inspect( c_47_c_77_vreg.v );
-    exit(1);
+	transpose( &( c_44_c_74_vreg.v ), &( c_45_c_75_vreg.v ),
+			   &( c_46_c_76_vreg.v ), &( c_47_c_77_vreg.v ) );
+	
+//	puts("----------------------");
+    
+//    inspect8( c_00_c_30_vreg.v, c_40_c_70_vreg.v );
+//    inspect8( c_01_c_31_vreg.v, c_41_c_71_vreg.v );
+//    inspect8( c_02_c_32_vreg.v, c_42_c_72_vreg.v );
+//    inspect8( c_03_c_33_vreg.v, c_43_c_73_vreg.v );
+//	
+//    inspect8( c_04_c_34_vreg.v, c_44_c_74_vreg.v );
+//    inspect8( c_05_c_35_vreg.v, c_45_c_75_vreg.v );
+//    inspect8( c_06_c_36_vreg.v, c_46_c_76_vreg.v );
+//    inspect8( c_07_c_37_vreg.v, c_47_c_77_vreg.v );
+//	exit(1);
 
     _mm256_storeu_pd( & C( 0, 0 ) ,  
 					_mm256_add_pd( _mm256_load_pd( & C( 0, 0 ) ) , c_00_c_30_vreg.v ) );
+    _mm256_storeu_pd( & C( 0, 4 ) ,  
+					_mm256_add_pd( _mm256_load_pd( & C( 0, 4 ) ) , c_04_c_34_vreg.v ) );
+
     _mm256_storeu_pd( & C( 1, 0 ) , 
 					_mm256_add_pd( _mm256_load_pd( & C( 1, 0 ) ) , c_01_c_31_vreg.v ) );
-    _mm256_storeu_pd( & C( 2, 0 ) , 
+    _mm256_storeu_pd( & C( 1, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 1, 4 ) ) , c_05_c_35_vreg.v ) );
+    
+	_mm256_storeu_pd( & C( 2, 0 ) , 
 					_mm256_add_pd( _mm256_load_pd( & C( 2, 0 ) ) , c_02_c_32_vreg.v ) );
-    _mm256_storeu_pd( & C( 3, 0 ) , 
+	_mm256_storeu_pd( & C( 2, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 2, 4 ) ) , c_06_c_36_vreg.v ) );
+    
+	_mm256_storeu_pd( & C( 3, 0 ) , 
 					_mm256_add_pd( _mm256_load_pd( & C( 3, 0 ) ) , c_03_c_33_vreg.v ) );
+	_mm256_storeu_pd( & C( 3, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 3, 4 ) ) , c_07_c_37_vreg.v ) );
 
+	_mm256_storeu_pd( & C( 4, 0 ) ,  
+					_mm256_add_pd( _mm256_load_pd( & C( 4, 0 ) ) , c_40_c_70_vreg.v ) );
+    _mm256_storeu_pd( & C( 4, 4 ) ,  
+					_mm256_add_pd( _mm256_load_pd( & C( 4, 4 ) ) , c_44_c_74_vreg.v ) );
 
+    _mm256_storeu_pd( & C( 5, 0 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 5, 0 ) ) , c_41_c_71_vreg.v ) );
+    _mm256_storeu_pd( & C( 5, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 5, 4 ) ) , c_45_c_75_vreg.v ) );
+    
+	_mm256_storeu_pd( & C( 6, 0 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 6, 0 ) ) , c_42_c_72_vreg.v ) );
+	_mm256_storeu_pd( & C( 6, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 6, 4 ) ) , c_46_c_76_vreg.v ) );
+    
+	_mm256_storeu_pd( & C( 7, 0 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 7, 0 ) ) , c_43_c_73_vreg.v ) );
+	_mm256_storeu_pd( & C( 7, 4 ) , 
+					_mm256_add_pd( _mm256_load_pd( & C( 7, 4 ) ) , c_47_c_77_vreg.v ) );
 }
 
